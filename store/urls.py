c1e68f7f0180014b9909fae0e751e6639cf1a214
Sumit <sumitscience2017@gmail.com>
2022-12-01 19:04:24 +0530

    first part of django-ecommerce project is completed

--- store/urls.py
+++ store/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.store),
-    path("cart/", views.cart),
-    path("checkout/", views.checkout),
+    path("", views.store, name="store"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
 ]
 
 
